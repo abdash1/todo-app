264b61c6f45caeb3182c373da517c8a8667c31a8
Abdullah Ashar <aasheikh@nisum.com>
2024-10-10 20:45:10 +0500

    todoapp using react

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todos => {
+      return  todos.id !== id;
+    });
+    this.setState({
+      todos
+    })
+  }
+
+  addtodo = (todo) => {
+    todo.id = Math.random();
+    let todos= [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
+
+  render(){
+    return (
+      <div className="todo-app container">
+        <h1 className='center blue text'>Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo  addtodo={this.addtodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
